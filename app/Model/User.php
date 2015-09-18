59cccf317453e7c73889cba418588ac152b6f0b0
ryosuke <ryosuke@wifi-139-191.sfc.wide.ad.jp>
2015-09-18 14:42:51 +0900

    スワップとかのあれ

--- app/Model/User.php
+++ app/Model/User.php
@@ -3,12 +3,4 @@ App::uses('AppModel', 'Model');
 App::uses('SimplePasswordHasher', 'Controller/Component/Auth');
 
 class User extends AppModel {
-
-/*  public function beforeSave($options = array()) {
-    if (isset($this->data[$this->alias]['password'])) {
-      $passwordHasher = new SimplePasswordHasher();
-      $this->data[$this->alias]['password'] = $passwordHasher->hash($this->data[$this->alias]['password']);
-    }
-    return true;
-  }*/
 }
